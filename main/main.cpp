a86207cae2815c94e6005007de022cd3f2832926
user <pinezhanin@yandex.ru>
2021-04-24 09:51:10 +0300

    n_sqr

--- main/main.cpp
+++ main/main.cpp
@@ -1,11 +1,34 @@
 #include<iostream>
-#include<add.h>
 
 using namespace std;
 
 int main() {
-	cout << "Hello!" << endl;
-	cout << add(1, 2) << endl;
+	int N;
+	int* array = new int[N];
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> array[i];
+	}
+	int count = 0;
+	for (int i = 0; i < N; i++) {
+		bool b = true;
+		for (int j = 0; j < N; j++) {
+			if (array[j] == i) {
+				b = false;
+				break;
+			}
+		}
+		int k = i;
+		int c = 1;
+		if (b) {
+			while (k != -1) {
+				k = array[k];
+				if (k != -1) c++;
+			}
+		}
+		if (c > count) count = c;
+	}
 	
+	cout << count << endl;
 	return 0;
 }
--- test/test_add.cpp
+++ test/test_add.cpp
@@ -1,10 +1,7 @@
-#include "add.h"
-
 #include <gtest.h>
 
 
 TEST(test_lib, simple_test)
 {
-  int v  = add(2,2);
-  EXPECT_EQ(4, v);
+  EXPECT_EQ(4, 4);
 }
